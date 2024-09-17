90139d448c40e1d6661d9ec1bfe12451259ac4b9
Artyom <artyom0104@gmail.com>
2024-09-17 16:40:14 +0300

    Оптимизация SELECT запроса

--- src/main/java/org/study/grabli_application/repository/StreetObjectRepository.java
+++ src/main/java/org/study/grabli_application/repository/StreetObjectRepository.java
@@ -1,7 +1,15 @@
 package org.study.grabli_application.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import org.study.grabli_application.entity.StreetObject;
 
+import java.util.List;
+
 public interface StreetObjectRepository extends JpaRepository<StreetObject, Long> {
+    @Override
+    @NonNull
+    @Query("select s from StreetObject s left join fetch s.type")
+    List<StreetObject> findAll();
 }
